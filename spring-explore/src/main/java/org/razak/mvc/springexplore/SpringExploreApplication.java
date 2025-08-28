91ee662d21e8f6695bcb86f613beeacbcb760654
razzaksr <razzaksr@gmail.com>
2025-08-28 10:39:47 +0530

    Spring intro and db setup

---
+++ spring-explore/src/main/java/org/razak/mvc/springexplore/SpringExploreApplication.java
@@ -0,0 +1,13 @@
+package org.razak.mvc.springexplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExploreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExploreApplication.class, args);
+    }
+
+}
---
+++ spring-explore/src/test/java/org/razak/mvc/springexplore/SpringExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.razak.mvc.springexplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExploreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
